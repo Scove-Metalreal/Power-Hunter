8721b08ea9f61da06c6adfa13a60e4fda27b1c49
emDuy <manhduyhoang90@gmail.com>
2025-10-23 10:29:49 +0700

    :))

--- Assets/_Project/_Scripts/Gameplay/Trap/RocketUp.cs
+++ Assets/_Project/_Scripts/Gameplay/Trap/RocketUp.cs
@@ -15,9 +15,9 @@ public class RocketUp : MonoBehaviour
     {
         
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             spawmRocket();
         }
